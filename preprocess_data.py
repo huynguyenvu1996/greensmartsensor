98ac9b3ec69099ab2ddc94ac7d59eb58af20b9c5
Nguyễn Vũ Huy <30389595+nguyenvuhuy@users.noreply.github.com>
2018-05-11 17:34:11 +0700

    Update preprocess_data.py

--- preprocess_data.py
+++ preprocess_data.py
@@ -1,29 +1,77 @@
-#https://machinelearningmastery.com/keras-functional-api-deep-learning/
-#https://stackoverflow.com/questions/17718271/python-count-for-multidimensional-arrays-list-of-lists
-#https://deeplearning.vn/2018/01/10/nhan-dien-chu-viet-bang-keras-phan-1/
-#https://stackoverflow.com/questions/44747343/keras-input-explanation-input-shape-units-batch-size-dim-etc
-
-import csv
 import numpy as np
-import operator
-from collections import Counter
+import glob
+import os
+import csv
 
-list = []
-with open('D:\Project\TimeSeriesDataForecast\dump_data\stores_sales\\135.csv', encoding='UTF-8', newline='') as csvfile:
-    spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
-    for row in spamreader:
-        list.append(row)
+WD = r"D:\Project\TimeSeriesDataForecast\dump_data\stores_sales"
 
-unique = []
-tmp = []
 
-for item in list:
-    tmp.append(item[2])
-unique = np.unique(tmp)
+class PreProcesssData:
+    def __init__(self):
+        pass
 
-print(unique)
+    def get_list_file_stores(self, wd, ext="*.csv", wf=False, path_file=os.getcwd(), file_name="list_file_stores_tmp"):
+        stores = glob.glob(os.path.join(wd, ext))
+        if (wf):
+            with open(path_file + file_name + ".csv", 'w',
+                      encoding='UTF-8', newline='') as outfile:
+                wr = csv.writer(outfile, delimiter=',')
+                wr.writerows(stores)
+        print("==>Done get list file stores")
+        return stores
 
-for item in unique:
-    s = sum(x.count(item) for x in list)
-    if(s > 350):
-        print(str(item) + ":" + str(s))
+    def get_list_category(self, list_file_stores, wd=os.getcwd(), min_day_numbers=350):
+        list_category_all_stores = []
+        for file in list_file_stores:
+            list_category_sales = []
+            list_id_category_tmp = []
+            list_category = []
+            list_id_category = None
+            print("==>Processing on file: \"" + file + "\" ")
+            with open(file, encoding='UTF-8',
+                      newline='') as csvfile:
+                spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+                for row in spamreader:
+                    list_category_sales.append(row)
+
+            for item in list_category_sales:
+                list_id_category_tmp.append(item[2])
+
+            list_id_category = np.unique(list_id_category_tmp)
+
+            for item in list_id_category:
+                s = sum(x.count(item) for x in list_category_sales)
+                if (s > min_day_numbers):
+                    list_category.append({'category_id': item, 'number_day': s})
+
+            print("-------------------------")
+            print('==>Store: ' + str(list_category_sales[0][0]))
+            if len(list_category) > 0:
+                with open(wd + str(list_category_sales[0][0]) + ".csv", 'w',
+                          encoding='UTF-8', newline='') as outfile:
+                    wr = csv.writer(outfile, delimiter=',')
+                    for item in list_category:
+                        wr.writerow([str(item['category_id']), str(item['number_day'])])
+                        print('Cat:' + str(item['category_id']) + "- Number day: " + str(item['number_day']))
+            print("-------------------------")
+
+        #     list_category_all_stores.append({'store_id': list_category_sales[0][0], 'list_category': list_category})
+        #
+        # for item in list_category_all_stores:
+        #     print("-------------------------")
+        #     print('==>Store: ' + str(item['store_id']))
+        #     with open(wd + str(item['store_id']) + ".csv", 'w',
+        #               encoding='UTF-8', newline='') as outfile:
+        #         wr = csv.writer(outfile, delimiter=',')
+        #         for cat in item['list_category']:
+        #             wr.writerow([str(cat['category_id']), str(cat['number_day'])])
+        #             print('Cat:' + str(cat['category_id']) + "- Number day: " + str(cat['number_day']))
+        #
+        #     print("-------------------------")
+
+
+prdata = PreProcesssData()
+list_file_stores = prdata.get_list_file_stores(WD)
+prdata.get_list_category(list_file_stores=list_file_stores,
+                         wd=r"D:\\Project\\TimeSeriesDataForecast\\dump_data\\category\\",
+                         min_day_numbers=350)
