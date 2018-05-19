b7cbf862221840ada9a2791ae70854fde203e332
Nguyễn Vũ Huy <30389595+nguyenvuhuy@users.noreply.github.com>
2018-05-19 19:18:38 +0700

    Update preprocess_data.py

--- preprocess_data.py
+++ preprocess_data.py
@@ -1,5 +1,6 @@
 https://medium.com/@jamsawamsa/running-a-google-cloud-gpu-for-fast-ai-for-free-5f89c707bae6
     http://omg-it.works/how-to-grab-video-frames-directly-from-qcamera/#acceptLicense
+        https://stackoverflow.com/questions/43529229/dlib-static-lib-error-in-debug-mode
 import numpy as np
 import glob
 import os
